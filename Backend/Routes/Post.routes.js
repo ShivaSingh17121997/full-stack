dbc30a1fffc36defb2a1d21a3edf70ddf4fe1090
ShivaSingh17121997 <shivasingh001997@gmail.com>
2023-06-12 15:56:40 +0530

    added 2

--- Backend/Routes/Post.routes.js
+++ Backend/Routes/Post.routes.js
@@ -1,5 +1,64 @@
 const express = require("express");
-const {PostModel} = require("../Models/Post.model")
-const socialRouter = express.Router();
+const { PostModel } = require("../Models/Post.model")
+const postRouter = express.Router();
 
 
+// Create post
+
+postRouter.post("./create", async (req, res) => {
+    try {
+        const post = PostModel(req.body);
+        await post.save();
+        res.status(200).send({ msg: "Post Added" })
+    } catch (error) {
+        res.status(400).send({ error: message });
+    }
+});
+
+postRouter.get("./", async (req, res) => {
+    try {
+        const { device, device1 } = req.query;
+        const query = { useerId: req.body, userId };
+        if (device) {
+            query.device = device;
+        }
+        if (device1) {
+            query.$and = [{ device: device }, { device: device }];
+        }
+        const post = await PostModel.find(query);
+        res.status(200).send({ msg: "All Posts!", post });
+    } catch (error) {
+        res.status(400).send({ error: error.message })
+    }
+});
+
+//update post
+postRouter.patch("./update/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updatePost = await PostModel.findByIdAndUpdate(
+            { _id: id },
+            req.body
+        );
+        res.status(200).send({ msg: "Post Updated", updatePost })
+    } catch (error) {
+        res.status(400).send({ error: error.message })
+
+    }
+})
+
+postRouter.post("./delete/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        await PostModel.findByIdAndDelete(
+            { _id: id });
+        res.status(200).send({ msg: "Post Updated", updatePost })
+    } catch (error) {
+        res.status(400).send({ error: error.message })
+
+    }
+})
+
+module.exports={
+    postRouter
+}
--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
 
 const {connection } = require("./Config/db")
+const {userRouter} = require("./Routes/User.routes")
+const {postRouter} = require("./Routes/Post.routes")
+const {Auth} = require
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-// app.listen() => {
+app.use("/users", userRouter);
+app.use(Auth);
+app.use("/posts", postRouter)
 
-// }
+app.listen(process.env.PORT, async()=>{
+    try{
+
+    } catch(error){
+        console.log(error);
+        console.log("Something wrong!");
+    }
+}) ;
